dafb3044b0755199ba57db315c229ae6760dbc6f
Roman007SFEDU <rn0361123@gmail.com>
2025-06-06 19:12:35 +0300

    Программа для определения варианта

---
+++ hello.py
@@ -0,0 +1,7 @@
+import random
+
+first_number = random.randint(0,3)
+second_number=random.randint(0,1)
+third_number=random.randint(0,2)
+
+print("Мой вариант: ",first_number,second_number,third_number)
